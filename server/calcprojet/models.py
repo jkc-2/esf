2c7ba059bb297bbc2f13a4569b430ac73149880f
Kévin Clément <kevin_clement@outlook.fr>
2023-12-11 00:25:47 +0100

    New tables

--- server/calcprojet/models.py
+++ server/calcprojet/models.py
@@ -9,3 +9,31 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+class MaterialType(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by_email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class MaterialCustomField(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by_email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class MaterialCustomFieldValue(models.Model):
+    value = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by_email = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.value
+
